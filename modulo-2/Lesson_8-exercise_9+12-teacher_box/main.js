2d75fd4a8d4cd91d888b96769d3e7764aaad08fe
alicia-colom <aliciacolom@gmail.com>
2020-10-15 13:33:07 +0200

    Add example 'Pepi' in exercise TeacherBox

--- modulo-2/Lesson_8-exercise_9+12-teacher_box/main.js
+++ modulo-2/Lesson_8-exercise_9+12-teacher_box/main.js
@@ -4,7 +4,7 @@
 
 // function handleChange() {
 // 	console.log("Soy la función y funciono");
-
+	
 // 	if (teacherBox.classList.contains("teacher--selected")) {
 // 		teacherBox.classList.remove("teacher--selected");
 // 		console.log("Estoy borrando la clase");
@@ -18,23 +18,41 @@
 
 //////
 
+//NO FUNCIONA!! NO ERES TÚ... :)
+// const pepi = document.querySelector(".pepi");
+
+// function handleChange(click) {
+// 	console.log("Soy la función y funciono");
+
+// 	if (click.pepi.classList.contains("teacher--selected")) {
+// 		click.pepi.classList.remove("teacher--selected");
+// 		console.log("Estoy borrando la clase");
+// 	} else {
+// 		click.pepi.classList.add("teacher--selected");
+// 		console.log("Estoy añadiendo la clase");
+// 	}
+// }
+
+// pepi.addEventListener("click", handleChange);
+
+//////
+
 const teacherBox = document.querySelectorAll(".teacher");
+// teacherBox = [teacherBox1, teacherBox2, teacherBox3];
+console.log(teacherBox);
 
 function handleChange(event) {
 	console.log("Soy la función y funciono");
-  // console.log(event);
-  // console.log(event.currentTarget);
 
 	if (event.currentTarget.classList.contains("teacher--selected")) {
 		event.currentTarget.classList.remove("teacher--selected");
-		console.log("Estoy borrando la clase");
+		console.log("IF: Estoy borrando la clase");
 	} else {
 		event.currentTarget.classList.add("teacher--selected");
-		console.log("Estoy añadiendo la clase");
+		console.log("ELSE: Estoy añadiendo la clase");
 	}
 }
 
 for (const eachTeacher of teacherBox) {
-	eachTeacher.addEventListener("click",handleChange);
+	eachTeacher.addEventListener("click", handleChange);
 }
-
